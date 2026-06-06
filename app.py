4d0da1105ece7601cd1890ddbf374ce7121a5150
sachinthokal <sachinthokal1799@gmail.com>
2026-06-06 16:31:10 +0000

    feat : new tool cron-expression-guru added

--- app.py
+++ app.py
@@ -3,17 +3,23 @@ from flask import Flask, render_template
 # Initialize the Flask application
 app = Flask(__name__)
 
-# Define the route for the home page (Root URL)
+# Homepage (Root URL)
 @app.route('/')
 def home():
     # Render the index.html file located in the 'templates' folder
     # Jinja2 will automatically handle the url_for('static', ...) links
     return render_template('index.html')
 
-# Define the route for the text case converter page
+# Text Case Converter
 @app.route('/text-case-converter/')
 def text_case_converter():
     return render_template('text-case-converter.html')
+
+# CRON GURU 
+@app.route('/cron-expression-guru/')
+def cron_expression_guru():
+    # Make sure the HTML file name matches exactly what you created
+    return render_template('cron-expression-guru.html')
 
 # Run the application in debug mode if this file is executed directly
 if __name__ == '__main__':
